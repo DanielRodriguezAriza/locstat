061c93052fca930e84890708e1e69c8ca50843b4
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-25 21:34:20 +0100

    Added functions to handle a path based on whether it is a file or a directory

--- locstat/locstat/Program.cs
+++ locstat/locstat/Program.cs
@@ -9,6 +9,54 @@ namespace locstat
             HandlePath(path);
         }
 
+        public void Handle(string path)
+        {
+            bool isDirectory = Directory.Exists(path);
+            bool isFile = File.Exists(path);
+
+            if (isDirectory)
+            {
+                HandleDirectory(path);
+                return;
+            }
+
+            if (isFile)
+            {
+                HandleFile(path);
+                return;
+            }
+
+            throw new Exception("The specified path does not exist!");
+        }
+
+
+        public void HandleDirectory(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            HandleDirectory(dir, null); // TODO : FIXME! don't pass null
+        }
+
+        public int HandleDirectory(DirectoryInfo directory, Dictionary<string, int> extensionData)
+        {
+            int lineCount = 0;
+
+            FileInfo[] files = directory.GetFiles();
+            foreach (var file in files)
+                lineCount += HandleFile(file, extensionData);
+
+            DirectoryInfo[] dirs = directory.GetDirectories();
+            foreach (var child in dirs)
+                lineCount += HandleDirectory(child, extensionData);
+
+            return lineCount;
+        }
+
+        public void HandleFile(string path)
+        {
+            FileInfo fileInfo = new FileInfo(path);
+            HandleFile(fileInfo, null); // TODO : FIXME!
+        }
+
         public int HandlePath(string path)
         {
             Dictionary<string, int> extensionData = new();
