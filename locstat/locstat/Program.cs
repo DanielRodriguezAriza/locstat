a14f5b9408a004f6407cda4fddc0ec2fe0453e65
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-25 16:48:59 +0100

    Added extension handling to the program

--- locstat/locstat/Program.cs
+++ locstat/locstat/Program.cs
@@ -1,39 +1,50 @@
-﻿namespace locstat
+﻿using System.Reflection;
+
+namespace locstat
 {
     public class LocStatHandler
     {
-        public LocStatHandler(string path)
+        public LocStatHandler(string path, string[] extensions)
         {
-            HandlePath(path);
+            HandlePath(path, extensions);
         }
 
-        public int HandlePath(string path)
+        public int HandlePath(string path, string[] extensions)
         {
             DirectoryInfo directory = new DirectoryInfo(path);
             Console.WriteLine($"Running locstat on path : \"{directory.FullName}\"");
-            int totalLinesCounted = HandleDir(directory);
+            int totalLinesCounted = HandleDir(directory, extensions);
             Console.WriteLine($"Total lines counted : {totalLinesCounted}");
             return totalLinesCounted;
         }
 
-        public int HandleDir(DirectoryInfo directory)
+        public int HandleDir(DirectoryInfo directory, string[] extensions)
         {
             int lineCount = 0;
 
             FileInfo[] files = directory.GetFiles();
             foreach (var file in files)
-                lineCount += HandleFile(file);
+                lineCount += HandleFile(file, extensions);
 
             DirectoryInfo[] dirs = directory.GetDirectories();
             foreach (var child in dirs)
-                lineCount += HandleDir(child);
+                lineCount += HandleDir(child, extensions);
 
             return lineCount;
         }
 
-        public int HandleFile(FileInfo fileInfo)
+        public int HandleFile(FileInfo fileInfo, string[] extensions)
         {
             int lineCount = 0;
+            bool canProcess = false;
+
+            foreach (var extension in extensions)
+                if (fileInfo.Name.EndsWith(extension))
+                    canProcess = true;
+
+            if (!canProcess)
+                return 0;
+
             try
             {
                 using (FileStream file = fileInfo.Open(FileMode.Open, FileAccess.Read))
@@ -59,7 +70,7 @@
     {
         static void Main(string[] args)
         {
-            LocStatHandler handler = new LocStatHandler("./");
+            LocStatHandler handler = new LocStatHandler("./", new string[] { "cs" });
         }
     }
 }
